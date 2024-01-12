ed2b8910a4e5c37edfaebb3a65d91694b87f8d5f
PatcityNJ <156215969+PatcityNJ@users.noreply.github.com>
2024-01-12 14:38:47 +0000

    remove footer 4

--- Hello.py
+++ Hello.py
@@ -28,6 +28,7 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            a {display:none; visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
